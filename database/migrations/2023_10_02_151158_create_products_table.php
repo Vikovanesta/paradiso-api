14b14bf4eb098a7c1c9a339cfc68abb9adabef12
Vikovanesta <Vikovanesta@gmail.com>
2023-10-02 22:27:00 +0700

    add product, productCategory, productSubcategory
    relations is not defined yet

---
+++ database/migrations/2023_10_02_151158_create_products_table.php
@@ -0,0 +1,48 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('merchant_id');
+            $table->foreignId('user_id');   // admin_id
+            $table->foreignId('product_sub_category_id');
+            $table->tinyInteger('status_id');
+            $table->foreignId('city_id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('duration');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('price');
+            $table->string('unit');
+            $table->integer('discount');
+            $table->string('thumbnail');
+            $table->string('address');
+            $table->string('coordinate');
+            $table->integer('max_person');
+            $table->integer('min_person');
+            $table->text('note');
+            $table->boolean('is_published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
