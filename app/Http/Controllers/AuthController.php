01f8bd61bb8a2afd866f2f4bc5749e9a881ea480
Vikovanesta <Vikovanesta@gmail.com>
2023-10-11 23:42:27 +0700

    add login form request
    - refactor validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,35 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthLoginRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Traits\HttpResponses;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
-use function Laravel\Prompts\error;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(Request $request)
+    public function login(AuthLoginRequest $request)
     {
-        // Validate the request...
-        $validated = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]);
+        $validated = $request->validated();
 
         $user = User::where('email', $validated['email'])->where('name', $validated['name'])->first();
-        if (!$user) {
-
-            return $this->error([],'User not found', 404);
-        }
-
-        if (!Hash::check($validated['password'], $user->password)) {
-            return $this->error([],'Password not match', 401);
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            throw new HttpResponseException(
+                response()->json([
+                    'message' => 'User not found',
+                    'errors' => [
+                        'message' => 'Incorrect name, email or password',
+                    ]
+                ], 401));
         }
 
         $token = $user->createToken('paradiso-token')->plainTextToken;
---
+++ app/Http/Requests/AuthLoginRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Http;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AuthLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     */
+    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator): void
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Validation error',
+                'errors' => $validator->getMessageBag(),
+            ], 422)
+        );
+    }
+}
